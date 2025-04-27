164c1a61f5e93374cfae761e189f29d32789edd3
PedroBettencourt <pedropbettencourt@gmail.com>
2025-04-27 11:49:01 +0000

    Finish styling

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -14,9 +14,9 @@ function App() {
     // Testing
     function handleExample() {
         setPersonalDetails({fullName: "Tomas Meadows", email:"tomas@meadows.com", phoneNumber: "123-456-789", address: "3820 Indiana Avenue"});
-        setDegrees([{id: crypto.randomUUID(), degree: "Sociology", school: "University of Sociology", startDate: "20-02-2010", endDate: "10-03-2013", location: "Lisbon"} ,
-            {id: crypto.randomUUID(), degree: "Political Science", school: "University of Porto", startDate: "20-09-2015", endDate: "10-10-2017", location: "Porto"}]);
-        setExperiences([{id: crypto.randomUUID(), name: "Wood Company", title: "Carpenter", startDate: "15-05-2018", endDate: "current", location: "Aveiro", description: "Working making furniture out of wood"}]);
+        setDegrees([{id: crypto.randomUUID(), degree: "Sociology", school: "University of Sociology", startDate: "02/2010", endDate: "03/2013", location: "Lisbon"} ,
+            {id: crypto.randomUUID(), degree: "Political Science", school: "University of Porto", startDate: "09/2015", endDate: "10/2017", location: "Porto"}]);
+        setExperiences([{id: crypto.randomUUID(), name: "Wood Company", title: "Carpenter", startDate: "05/2018", endDate: "present", location: "Aveiro", description: "Working making furniture out of wood"}]);
     }
 
     function handleDelete() {
